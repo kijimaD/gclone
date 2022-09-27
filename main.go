d93a7a0ae2857df09dc04377e3b9eac494c42fe3
kijimaD <norimaking777@gmail.com>
2022-09-27 23:29:50 +0900

    refactor: split command file

--- main.go
+++ main.go
@@ -3,13 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/briandowns/spinner"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
-	"time"
-	"unicode/utf8"
 )
 
 const (
@@ -28,13 +21,13 @@ type group struct {
 	Repos []string `yaml:"repos"`
 }
 
-type result struct {
-	lines []string
-}
-
 type outputBuilder struct {
 	config *config
 	result *result
+}
+
+type result struct {
+	lines []string
 }
 
 func newOutputBuilder(config *config, result *result) *outputBuilder {
@@ -50,23 +43,8 @@ func (o *outputBuilder) writeResult() {
 	}
 }
 
-type commandBuilder struct {
-	config *config
-	output *outputBuilder
-	group  group
-}
-
-func newCommandBuilder(config *config, output *outputBuilder, group group) *commandBuilder {
-	return &commandBuilder{
-		config,
-		output,
-		group,
-	}
-}
-
 func main() {
 	config, _ := LoadConfigForYaml()
-
 	flag.Parse()
 	subCommand := flag.Arg(0)
 
@@ -89,53 +67,4 @@ func main() {
 
 // 結果の構造体を作って、そこに結果を入れたい。そしてまとめた箇所で出力したい。今は出力がバラバラ
 // オプションと結果を保持する構造体・メソッドを作っていく
-
 // コマンドオプションと、ymlから読み取る設定をマージする
-
-func (c commandBuilder) execute() {
-	c.moveDir()
-	c.showInfo()
-	c.executeCommand()
-}
-
-func (c commandBuilder) moveDir() {
-	absPath, _ := filepath.Abs(ExpandHomedir(c.group.Dest))
-	dirErr := os.Chdir(absPath)
-	if dirErr != nil {
-		panic(dirErr)
-	}
-}
-
-func (c commandBuilder) showInfo() {
-	path, _ := os.Getwd()
-	targetDir := fmt.Sprintf("Target dir: %v", path)
-	reposCount := fmt.Sprintf("Repo count: %v", len(c.group.Repos))
-
-	line := strings.Repeat("─", utf8.RuneCountInString(path))
-
-	fmt.Println(line)
-	fmt.Println(targetDir)
-	fmt.Println(reposCount)
-	fmt.Println(line)
-}
-
-func (c commandBuilder) executeCommand() {
-	s := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
-	s.Start()
-	defer s.Stop()
-
-	for _, repo := range c.group.Repos {
-		_, err := exec.Command(mainGitCommand, buildCommand(repo)...).Output()
-		if err != nil {
-			line := fmt.Sprintf("❌ %s \n ↪ %s", repo, err.Error())
-			c.output.result.lines = append(c.output.result.lines, line)
-		} else {
-			c.output.result.lines = append(c.output.result.lines, fmt.Sprintf("✔ %s", repo))
-		}
-	}
-}
-
-func buildCommand(repo string) []string {
-	command := []string{subGitCommand, repo}
-	return command
-}
