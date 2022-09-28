ce7b4c819d9a3edc3832bdd04cce5223076a9632
kijimaD <norimaking777@gmail.com>
2022-09-28 12:13:19 +0900

    refactor: group function

--- command.go
+++ command.go
@@ -11,6 +11,14 @@ import (
 	"unicode/utf8"
 )
 
+const (
+	mainGitCommand    = "git"
+	subGitCommand     = "clone"
+	defaultConfigPath = "config.yml"
+	homeDir           = "~/"
+	lineChar          = "─"
+)
+
 type commandBuilder struct {
 	config *config
 	output *outputBuilder
@@ -25,10 +33,14 @@ func newCommandBuilder(config *config, output *outputBuilder, group group) *comm
 	}
 }
 
-func (c commandBuilder) execute() {
+func (c commandBuilder) executeGroup() {
 	c.moveDir()
 	c.groupInfo()
-	c.executeCommand()
+
+	for _, repo := range c.group.Repos {
+		c.executeCommand(repo)
+	}
+	c.output.appendProgress("") // newline
 }
 
 func (c commandBuilder) moveDir() {
@@ -43,7 +55,7 @@ func (c commandBuilder) groupInfo() {
 	currentPath, _ := os.Getwd()
 	targetDir := fmt.Sprintf("Target dir: %v", currentPath)
 	reposCount := fmt.Sprintf("Repo count: %v", len(c.group.Repos))
-	line := strings.Repeat("─", utf8.RuneCountInString(targetDir))
+	line := strings.Repeat(lineChar, utf8.RuneCountInString(targetDir))
 
 	c.output.appendProgress(line)
 	c.output.appendProgress(targetDir)
@@ -52,23 +64,20 @@ func (c commandBuilder) groupInfo() {
 	c.output.writeProgress()
 }
 
-func (c commandBuilder) executeCommand() {
+func (c commandBuilder) executeCommand(repo string) {
 	s := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
 	s.Start()
 	defer s.Stop()
 
-	for _, repo := range c.group.Repos {
-		_, err := exec.Command(mainGitCommand, []string{subGitCommand, repo}...).Output()
-		if err != nil {
-			line := fmt.Sprintf("❌ %s \n ↪ %s", repo, err.Error())
-			c.output.appendProgress(line)
-			c.output.fail++
-		} else {
-			line := fmt.Sprintf("✔ %s", repo)
-			c.output.appendProgress(line)
-			c.output.success++
-		}
-		c.output.writeProgress()
+	_, err := exec.Command(mainGitCommand, []string{subGitCommand, repo}...).Output()
+	if err != nil {
+		line := fmt.Sprintf("❌ %s \n ↪ %s", repo, err.Error())
+		c.output.appendProgress(line)
+		c.output.fail++
+	} else {
+		line := fmt.Sprintf("✔ %s", repo)
+		c.output.appendProgress(line)
+		c.output.success++
 	}
-	c.output.appendProgress("") // newline
+	c.output.writeProgress()
 }
